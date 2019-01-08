94d883d6ad57f3074a032865d0eb9986d9804b49
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-01-08 13:17:12 -0500

    updating schedule app js as I go

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,4 +13,87 @@ var config = {
 firebase.initializeApp(config);
 
 // Create a variable to reference the database
-var database = firebase.database();
+var dataRef = firebase.database();
+
+// variables
+
+var trainName = "";
+var destination = "";
+var firstTime = 0;
+var frequency = 0;
+
+$("#addTrainBtn").on("click", function(){
+    event.preventDefault();
+    //console.log("Clicked")
+    
+    trainName = $("#trainName").val().trim();
+    destination = $("#destination").val().trim();
+    firstTime = $("#firstTime").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTime);
+    console.log(frequency);
+
+    var newTrain = {
+        trainName: trainName,
+        destination: destination,
+        firstTime: firstTime,
+        frequency: frequency
+    }
+
+    console.log(newTrain);
+
+    dataRef.ref().push(newTrain);
+
+    // clear inputs
+    $("#trainName").val("");
+    $("#destination").val("");
+    $("#firstTime").val("");
+    $("#frequency").val(0);
+});
+
+    // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+    dataRef.ref().on("child_added", function(childSnapshot) {
+
+        // Log everything that's coming out of snapshot
+        console.log("childsnapshot" + childSnapshot);
+        console.log(childSnapshot.val().trainName);
+        console.log(childSnapshot.val().destination);
+        console.log(childSnapshot.val().firstTime);
+        console.log(childSnapshot.val().frequency);
+
+		var newName = childSnapshot.val().trainName;
+        var newDestination = childSnapshot.val().destination;
+        var newTrainTime = childSnapshot.val().firstTime;
+		var newFrequency = childSnapshot.val().frequency;
+
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(newTrainTime, "HH:mm").subtract(1, "years");
+        console.log("CONV TRAIN TIME: " + firstTimeConverted);
+
+        // Current Time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        // Difference between the times
+        var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + timeDiff);
+
+        // Time apart (remainder)
+        var timeRemainder = timeDiff % newFrequency;
+        console.log("TIME REMAINDER: " + timeRemainder);
+
+        //minutes until  train
+        var nextTrainMin = newFrequency - timeRemainder;
+        console.log("MINUTES TILL TRAIN: " + nextTrainMin);
+
+        //next train
+        var nextTrainAdd = moment().add(nextTrainMin, "minutes");
+        var nextTrainArr = moment(nextTrainAdd).format("HH:mm");
+        console.log("ARRIVAL TIME: " + nextTrainArr);
+        
+
+
+    });
